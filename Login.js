b31676a616dcb045d7e1fb199c2dc61241f3956a
ALCHACAS <chavesa698@gmail.com>
2023-02-28 16:55:11 -0600

    cositas

---
+++ Login.js
@@ -0,0 +1,14 @@
+function validarUsuario() {
+  var usuario = document.getElementById("usuario").value;
+  var contraseña = document.getElementById("contraseña").value;
+  if (usuario.trim() == "" || contraseña.trim() == "") {
+    alert("Por favor ingrese su usuario y contraseña");
+    return false;
+  }
+  alert("Bienvenido " + usuario);
+  window.location.href = "index.html";
+  return false;
+}
+function alerta(){
+  alert("Bienvenido " + "a");
+}
