b806055d264a48790b8c1bf21863af3ae02780c6
ALCHACAS <alvaro.chaves.cascante@est.una.ac.cr>
2023-03-13 00:50:32 -0600

    terminado creo

--- Login.js
+++ Login.js
@@ -13,7 +13,23 @@ function alerta() {
   alert("Bienvenido " + "a");
 }
 function redirigirAIndex() {
-  window.location.href = 'index.html';
+  window.location.href = "index.html";
 }
-
-
+function registroInfo() {
+  var nombre = document.getElementById("Nombre").value;
+  var apellido = document.getElementById("Apellido").value;
+  var usuario = document.getElementById("Usuario").value;
+  var correo = document.getElementById("Correo").value;
+  var confirCorreo = document.getElementById("ConfirmarCorreo").value;
+  if (
+    nombre.trim() == "" ||
+    apellido.trim() == "" ||
+    usuario.trim() == "" ||
+    correo.trim() == "" ||
+    confirCorreo.trim() == ""
+  ) {
+    alert("Favor ingresar todos los datos");
+  } else {
+    alert("Datos registrados correctamente");
+  }
+}
